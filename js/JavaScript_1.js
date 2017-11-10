be6c00562c91de67b32a2276ee77ca75ec29e7f9
miaochunchen2017 <miaochunchen2011@163.coom>
2017-11-10 09:36:05 +0800

    9:35 2017-11-10

--- js/JavaScript_1.js
+++ js/JavaScript_1.js
@@ -3,3 +3,15 @@ function test_1() {
         document.write("number is "+i+"<br>");
     }
 }
+
+function test_2() {
+    for (i = 1; i < 10; i++) {
+        if (i == 5 || i == 6) {
+            continue;
+        }
+        if (i == 8) {
+            break;
+        }
+        document.write(i);
+    }
+}
